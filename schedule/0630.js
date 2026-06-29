acae0a92c6fcc989cca181a0eab28b5304dbce52
Hangbo <hangbo@192.168.0.101>
2026-06-30 03:24:03 +0800

    📊 比分更新:  巴西 2-1 日本  南非 0-1 加拿大

--- schedule/0630.js
+++ schedule/0630.js
@@ -25,8 +25,8 @@ SCHEDULE_DATA['0630'] = `
                     <span class="match-group">1/16决赛</span>
                     <span class="match-team">巴西</span>
                     <span class="match-team">日本</span>
-                    <span class="match-score">—</span>
-                    <span class="match-score">—</span>
+                    <span class="match-score">2</span>
+                    <span class="match-score">1</span>
                     <a href="predictions/0630.html" class="pred-link">📊</a>
                 </div>
             </div>
